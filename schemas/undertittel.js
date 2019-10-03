93efa34a668e39b2963e0d7d95c4e32399e9cb7e
Asbjørn Steinskog <asbjorn@steinskog.me>
2019-10-03 13:11:33 +0200

    Bytt rekkefølge på undertittel i panel og liste
    Sånn at ikke undertittelen skal vises som 'Untitled' om man bare har undertittel i panel

--- schemas/undertittel.js
+++ schemas/undertittel.js
@@ -4,14 +4,14 @@ export default {
   title: 'Undertittel',
   fields: [
     {
-      name: 'tekst_i_liste',
-      type: 'string',
-      title: 'Undertittel som vises i liste',
-    },
-    {
       name: 'tekst_i_panel',
       type: 'string',
       title: 'Undertittel som vises i panel',
+    },
+    {
+      name: 'tekst_i_liste',
+      type: 'string',
+      title: 'Undertittel som vises i liste',
     },
     {
       name: 'brodtekster',
